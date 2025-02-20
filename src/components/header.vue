bd20e910ef57f5c111e8045fcfe65d4834771352
Feres Henteti <fereshenteti@gmail.com>
2025-02-20 02:32:30 +0100

    fixed nav-menu items highlight when selected

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,12 +1,16 @@
 <script setup>
-    import { ref, watch } from "vue";
+    import { ref, watch, onMounted, onUnmounted } from "vue";
     import { useI18n } from "vue-i18n";
     import { ROUTE_CONSTS } from '@/router.js';
     import customSelect from "@/components/customSelect.vue";
     import { store } from '@/store';
+    import { useRoute } from "vue-router";
 
     const { t, availableLocales } = useI18n();
     const logo = new URL('/src/assets/LogoFormatic.png', import.meta.url).href
+    
+    const route = useRoute();
+    const currentHash = ref(route.hash);
 
     const selectedLang = ref(store.lang);
     const langOptions = ref(availableLocales.map(locale => {
@@ -21,9 +25,17 @@
         isMenuOpen.value = !isMenuOpen.value;
     };
 
+    const isActive = (hash) => {
+        return currentHash.value.endsWith(hash);
+    };
+
     watch(selectedLang, () => {
         store.changeLang(selectedLang.value);
     });
+
+    watch(() => route.fullPath, (newHash) => {
+        currentHash.value = newHash;
+    })
 
 </script>
 
@@ -47,22 +59,22 @@
 
             <BNav v-if="!isMenuOpen" class="header-left">
                 <li class="nav-item">
-                    <RouterLink :to="'/#whoAreWe'" class="nav-link">
+                    <RouterLink :to="'/#whoAreWe'" :class="`nav-link ${isActive('#whoAreWe') ? 'formatik-active-link' : ''}`">
                     {{ t('HEADER_NAV_WHO_ARE_WE') }}
                     </RouterLink>
                 </li>
                 <li class="nav-item">
-                    <RouterLink :to="'/#ourServices'" class="nav-link">
+                    <RouterLink :to="'/#ourServices'" :class="`nav-link ${isActive('#ourServices') ? 'formatik-active-link' : ''}`">
                     {{ t('HEADER_NAV_WHAT_DO_WE_OFFER') }}
                     </RouterLink>
                 </li>
                 <li class="nav-item">
-                    <RouterLink :to="'/#howToUse'" class="nav-link">
+                    <RouterLink :to="'/#howToUse'" :class="`nav-link ${isActive('#howToUse') ? 'formatik-active-link' : ''}`">
                     {{ t('HEADER_NAV_HOW_TO_USE') }}
                     </RouterLink>
                 </li>
                 <li class="nav-item">
-                    <RouterLink :to="ROUTE_CONSTS.OFFERS" class="nav-link">
+                    <RouterLink :to="ROUTE_CONSTS.OFFERS" :class="`nav-link ${isActive(ROUTE_CONSTS.OFFERS) ? 'formatik-active-link' : ''}`">
                         {{ t('HEADER_NAV_OUR_OFFERS') }}
                     </RouterLink>
                 </li>
@@ -83,22 +95,22 @@
         <div :class="`mobile-menu ${isMenuOpen ? 'open-menu' : ''}`">
             <BNav class="mobile-menu-wrapper">
                 <li class="nav-item" v-on:click="isMenuOpen = false">
-                    <RouterLink :to="'/#whoAreWe'" class="nav-link">
+                    <RouterLink :to="'/#whoAreWe'" :class="`nav-link ${isActive('#whoAreWe') ? 'formatik-active-link' : ''}`">
                     {{ t('HEADER_NAV_WHO_ARE_WE') }}
                     </RouterLink>
                 </li>
                 <li class="nav-item" v-on:click="isMenuOpen = false">
-                    <RouterLink :to="'/#ourServices'" class="nav-link">
+                    <RouterLink :to="'/#ourServices'" :class="`nav-link ${isActive('#ourServices') ? 'formatik-active-link' : ''}`">
                     {{ t('HEADER_NAV_WHAT_DO_WE_OFFER') }}
                     </RouterLink>
                 </li>
                 <li class="nav-item" v-on:click="isMenuOpen = false">
-                    <RouterLink :to="'/#howToUse'" class="nav-link">
+                    <RouterLink :to="'/#howToUse'" :class="`nav-link ${isActive('#howToUse') ? 'formatik-active-link' : ''}`">
                     {{ t('HEADER_NAV_HOW_TO_USE') }}
                     </RouterLink>
                 </li>
                 <li class="nav-item" v-on:click="isMenuOpen = false">
-                    <RouterLink :to="ROUTE_CONSTS.OFFERS" class="nav-link">
+                    <RouterLink :to="ROUTE_CONSTS.OFFERS" :class="`nav-link ${isActive(ROUTE_CONSTS.OFFERS) ? 'formatik-active-link' : ''}`">
                         {{ t('HEADER_NAV_OUR_OFFERS') }}
                     </RouterLink>
                 </li>
